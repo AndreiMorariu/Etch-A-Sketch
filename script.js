bbe889c3b5de3989d298a38a91b0284e5e724006
AndreiMorariu <andreimorariu4140@gmail.com>
2022-11-17 00:16:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const container = document.querySelector(".container");
+const btnChange = document.querySelector(".change");
+const btnReset = document.querySelector(".reset");
+
+const generateGrid = function (size = 16) {
+  container.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`
+  );
+  for (let i = 0; i < size * size; i++) {
+    const element = document.createElement("div");
+    element.classList.add("element");
+    container.appendChild(element);
+    element.addEventListener("mouseover", function () {
+      element.style.backgroundColor = "#000";
+    });
+  }
+};
+
+generateGrid();
+
+btnChange.addEventListener("click", function () {
+  container.innerHTML = "";
+  const size = prompt("Enter the size for your grid");
+  generateGrid(size);
+});
+
+btnReset.addEventListener("click", function () {
+  location.reload();
+});
